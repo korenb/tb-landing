2b5f6eaee37005e1b4ea70e7dc0013d7298d2036
Sergey <korenb23@mail.ru>
2017-09-02 22:05:18 +0500

    fixes #3

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
-gulp.task('css:build', function() {
+gulp.task('compile:scss', function() {
     gulp.src('./src/styles/styles.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./Content/css'));
 });
 
-gulp.task("watch", function () {
-    return gulp.watch('./src/styles/*.scss', ["css:build"]);
+gulp.task('min:css', ['compile:scss'], function(){
+    return gulp.src('./Content/css/styles.css')
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./Content/css'));
+});
+
+gulp.task('watch', function () {
+    return gulp.watch('./src/styles/*.scss', ['compile:scss']);
 });
